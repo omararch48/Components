dc052bc0065bb5831f7ec2529e1606bc9ed6016d
Omar <omardanielesquivel@gmail.com>
2022-08-19 04:14:47 -0500

    planet scroll

--- js/app_parallax.js
+++ js/app_parallax.js
@@ -6,19 +6,23 @@
 
     const parallaxOne = document.querySelector('.parallax');
     const parallaxPlanet = document.querySelector('.parallax__planet');
+    const stars = document.querySelector('.stars');
 
     let mainUbication = window.scrollY;
+    // parallaxPlanet.style.top = '-1200px';
+    // parallaxPlanet.style.left = '-800px';
+    // parallaxPlanet.style.display = 'block';
 
     window.onscroll = () => {
-
-        
         let displacement = window.scrollY.toPrecision();
         parallaxOne.style.bottom = `calc(20vh - ${(displacement * 2)}px)`;
-        parallaxPlanet.style.top = `${(displacement * 1.02)}px`;
-        parallaxPlanet.style.left = `${(displacement * 1.2)}px`;
+        parallaxPlanet.style.top = `${(displacement * 1.35) - 1200}px`;
+        parallaxPlanet.style.left = `${(displacement * 0.3) - 800}px`;
 
 
-
+        if (displacement > 0) {
+            stars.style.overflow = 'hidden'
+        }
 
 
         if (mainUbication >= displacement) {
